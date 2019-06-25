dd3148668374b4dddc180e1d2eafcd91e5bfdeff
Adrianna Valdivia <adrianna.valdivia@salesloft.com>
2019-06-25 14:19:35 -0400

    change to url.

--- gatsby-config.js
+++ gatsby-config.js
@@ -156,7 +156,7 @@ module.exports = {
         serialize: ({ site, allSitePage }) =>
           allSitePage.edges.map(edge => {
             return {
-              siteUrl: site.siteMetadata.siteUrl + edge.node.path,
+              url: site.siteMetadata.siteUrl + edge.node.path,
               changefreq: 'daily',
               priority: 0.7,
             }

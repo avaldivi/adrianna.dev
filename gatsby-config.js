d07f26b2b3fe3565a79a0870869800af00492a8c
Adrianna Valdivia <adrianna.valdivia@salesloft.com>
2019-06-30 19:05:44 -0400

    Add basic and twitter meta tags, update siteMetaData.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,49 +1,53 @@
-const lost = require('lost')
-const pxtorem = require('postcss-pxtorem')
+const lost = require("lost")
+const pxtorem = require("postcss-pxtorem")
 
 module.exports = {
   siteMetadata: {
-    siteUrl: 'https://adriannavaldivia.netlify.com',
-    url: 'https://adriannavaldivia.netlify.com',
-    title: 'Adrianna Valdivia',
-    subtitle:
-      'Speaker, Writer, and UI Engineer',
-    copyright: '© All rights reserved.',
-    disqusShortname: '',
+    siteUrl: "https://adriannavaldivia.netlify.com",
+    url: "https://adriannavaldivia.netlify.com",
+    title: "adrianna.dev",
+    subtitle: "Speaker, Writer, and UI Engineer",
+    description: "Speaker, Writer, and UI Engineer",
+    handle: "@adriannavaldivi",
+    image: "src/assets/images/girl-boss.jpg",
+    keywords: ["technology blog", "ui engineer", "javascript"],
+    meta: "",
+    copyright: "© All rights reserved.",
+    disqusShortname: "",
     menu: [
       {
-        label: 'Articles',
-        path: '/',
+        label: "Articles",
+        path: "/",
       },
       {
-        label: 'About me',
-        path: '/about/',
+        label: "About me",
+        path: "/about/",
       },
       {
-        label: 'Contact me',
-        path: '/contact/',
+        label: "Contact me",
+        path: "/contact/",
       },
     ],
     author: {
-      name: 'Adrianna Valdivia',
-      email: 'adrianna.valdivia@gmail.com',
-      telegram: '',
-      twitter: 'adriannavaldivi',
-      github: 'avaldivi',
-      rss: 'rss',
-      linkedin: 'adriannavaldivia',
+      name: "Adrianna Valdivia",
+      email: "adrianna.valdivia@gmail.com",
+      telegram: "",
+      twitter: "adriannavaldivi",
+      github: "avaldivi",
+      rss: "rss",
+      linkedin: "adriannavaldivia",
     },
   },
   plugins: [
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages',
+        name: "pages",
       },
     },
     {
-      resolve: 'gatsby-plugin-feed',
+      resolve: "gatsby-plugin-feed",
       options: {
         query: `
           {
@@ -65,7 +69,7 @@ module.exports = {
                   date: edge.node.frontmatter.date,
                   url: site.siteMetadata.site_url + edge.node.fields.slug,
                   guid: site.siteMetadata.site_url + edge.node.fields.slug,
-                  custom_elements: [{ 'content:encoded': edge.node.html }],
+                  custom_elements: [{ "content:encoded": edge.node.html }],
                 })
               ),
             query: `
@@ -93,45 +97,45 @@ module.exports = {
                 }
               }
             `,
-            output: '/rss.xml',
+            output: "/rss.xml",
           },
         ],
       },
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
               maxWidth: 960,
             },
           },
           {
-            resolve: 'gatsby-remark-responsive-iframe',
-            options: { wrapperStyle: 'margin-bottom: 1.0725rem' },
+            resolve: "gatsby-remark-responsive-iframe",
+            options: { wrapperStyle: "margin-bottom: 1.0725rem" },
           },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-smartypants",
         ],
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: { trackingId: 'UA-73379983-2' },
+      resolve: "gatsby-plugin-google-analytics",
+      options: { trackingId: "UA-73379983-2" },
     },
     {
-      resolve: 'gatsby-plugin-google-fonts',
+      resolve: "gatsby-plugin-google-fonts",
       options: {
-        fonts: ['roboto:400,400i,500,700'],
+        fonts: ["roboto:400,400i,500,700"],
       },
     },
     {
-      resolve: 'gatsby-plugin-sitemap',
+      resolve: "gatsby-plugin-sitemap",
       options: {
         query: `
             {
@@ -152,22 +156,22 @@ module.exports = {
                 }
               }
           }`,
-        output: '/sitemap.xml',
+        output: "/sitemap.xml",
         serialize: ({ site, allSitePage }) =>
           allSitePage.edges.map(edge => {
             return {
               url: site.siteMetadata.siteUrl + edge.node.path,
-              changefreq: 'daily',
+              changefreq: "daily",
               priority: 0.7,
             }
           }),
       },
     },
-    'gatsby-plugin-offline',
-    'gatsby-plugin-catch-links',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-offline",
+    "gatsby-plugin-catch-links",
+    "gatsby-plugin-react-helmet",
     {
-      resolve: 'gatsby-plugin-sass',
+      resolve: "gatsby-plugin-sass",
       options: {
         postCssPlugins: [
           lost(),
@@ -175,23 +179,23 @@ module.exports = {
             rootValue: 16,
             unitPrecision: 5,
             propList: [
-              'font',
-              'font-size',
-              'line-height',
-              'letter-spacing',
-              'margin',
-              'margin-top',
-              'margin-left',
-              'margin-bottom',
-              'margin-right',
-              'padding',
-              'padding-top',
-              'padding-left',
-              'padding-bottom',
-              'padding-right',
-              'border-radius',
-              'width',
-              'max-width',
+              "font",
+              "font-size",
+              "line-height",
+              "letter-spacing",
+              "margin",
+              "margin-top",
+              "margin-left",
+              "margin-bottom",
+              "margin-right",
+              "padding",
+              "padding-top",
+              "padding-left",
+              "padding-bottom",
+              "padding-right",
+              "border-radius",
+              "width",
+              "max-width",
             ],
             selectorBlackList: [],
             replace: true,
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
-import Post from '../components/Post'
-import Sidebar from '../components/Sidebar'
+import React from "react"
+import Helmet from "react-helmet"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import Post from "../components/Post"
+import Sidebar from "../components/Sidebar"
 
 class IndexRoute extends React.Component {
   render() {
     const items = []
-    const { title, subtitle } = this.props.data.site.siteMetadata
+    const {
+      title,
+      subtitle,
+      description,
+      handle,
+      site,
+      image,
+      keywords,
+      meta,
+    } = this.props.data.site.siteMetadata
     const posts = this.props.data.allMarkdownRemark.edges
     posts.forEach(post => {
       items.push(<Post data={post} key={post.node.fields.slug} />)
@@ -17,7 +26,64 @@ class IndexRoute extends React.Component {
     return (
       <Layout>
         <div>
-          <Helmet>
+          <Helmet
+            htmlAttributes={{
+              lang: this.props.lang,
+            }}
+            title={title}
+            titleTemplate={`%s | ${title}`}
+            meta={[
+              {
+                name: "description",
+                content: description,
+              },
+              {
+                property: "og:title",
+                content: title,
+              },
+              {
+                property: "og:description",
+                content: description,
+              },
+              {
+                property: "og:type",
+                content: "website",
+              },
+              {
+                name: "twitter:card",
+                content: "summary",
+              },
+              {
+                name: "twitter:creator",
+                content: handle,
+              },
+              {
+                name: "twitter:site",
+                content: site,
+              },
+              {
+                name: "twitter:title",
+                content: title,
+              },
+              {
+                name: "twitter:description",
+                content: description,
+              },
+              {
+                name: "twitter:image",
+                content: image,
+              },
+            ]
+              .concat(
+                keywords.length > 0
+                  ? {
+                      name: "keywords",
+                      content: keywords.join(", "),
+                    }
+                  : []
+              )
+              .concat(meta)}
+          >
             <title>{title}</title>
             <meta name="description" content={subtitle} />
           </Helmet>
@@ -39,7 +105,12 @@ export const pageQuery = graphql`
       siteMetadata {
         title
         subtitle
+        description
         copyright
+        keywords
+        handle
+        image
+        meta
         menu {
           label
           path
